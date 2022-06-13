604894c89480008b2c715c9d8cfd42ae4371b77d
Lethaha <lethha.thamae@gmail.com>
2022-06-13 22:46:14 +0200

    create a service implementation to define the functionality for my server app

--- src/main/java/com/lethaha/serverbackend/service/ServerService.java
+++ src/main/java/com/lethaha/serverbackend/service/ServerService.java
@@ -2,12 +2,14 @@ package com.lethaha.serverbackend.service;
 
 import com.lethaha.serverbackend.model.Server;
 
+import java.io.IOException;
 import java.util.Collection;
 
 public interface ServerService {
     Server create(Server server);
+    Server ping(String ipAddress) throws IOException;
     Collection<Server> list(int limit);
     Server get(Long id);
     Server update(Server server);
-    Server delete(Long id);
+    Boolean delete(Long id);
 }
