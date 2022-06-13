c6865beb4c81dd339f259015ca9bcb9112363498
Lethaha <lethha.thamae@gmail.com>
2022-06-13 19:34:22 +0200

    Initialized springboot project with spring web, lambok, validation, spring data jpa and MySQL driver

---
+++ src/main/java/com/lethaha/serverbackend/ServerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lethaha.serverbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerBackendApplication.class, args);
+	}
+
+}
